b276b4c90c04c7a9ee1734c8a16bde26aed5421e
mururu <yuki@gnnk.net>
2012-06-05 16:54:42 +0900

    fix error message

--- lib/amida/label.rb
+++ lib/amida/label.rb
@@ -1,7 +1,7 @@
 module Amida
   class Label
     def initialize(width)
-      raise ArgumentError, "Width of Amida has to be 27 and under" if width > 26
+      raise ArgumentError, "Width of Amida has to be 26 and under" if width > 26
       @content = ('A'..'Z').to_a[0..width-1]
     end
 
